e40cedd11e6ede875a1e9110962f78284c9ed0a0
Frank McNulty <f-mcnulty@hotmail.com>
2021-01-20 23:31:33 +0000

    settings

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -18,17 +18,21 @@ import { Button,
   Container,
   Message,
 Popup,
-Icon} from 'semantic-ui-react'
+Icon,
+Checkbox,
+Dropdown,
+Table} from 'semantic-ui-react'
 
   
 
 
 const Home = () => {
-  const [openModal, setOpenModal] = useState(false);
-  const [percentage, setPercentage] = useState(10);
+  const [open, setOpen] = useState(false);
+  const [percentage, setPercentage] = useState(5);
   const [amount, setAmount] = useState(0);
   const [people, setPeople] = useState(0);
   const [showBill, setShowBill] = useState(0);
+  const [defaultCurrency, setDefaultCurrency] = useState('£')
   let number = Array.from(Array(20).keys())
   useEffect(() => {
     setPeople()
@@ -43,9 +47,9 @@ const Home = () => {
     setShowBill()
     console.log(showBill);
   }, [])
-
-
-
+  useEffect(() => {
+    setDefaultCurrency('£');
+  }, [])
 
   const increasePercentageTip = () => {
     setPercentage(count => count + 1);
@@ -56,29 +60,13 @@ const Home = () => {
   }
   
   
- 
 
   const caculateBill = (bill, tip) => {
       const numberOfPeople = parseInt(people.target.value)
-
       const billAndTipTotal =  parseInt(amount.target.value) + parseInt(amount.target.value) * percentage/100;
       return setShowBill(billAndTipTotal / parseInt(people.target.value).toFixed(2))
-
-      // console.log(billAndTipTotal / parseInt(people.target.value));
     }
 
-    
-      // return billAndTipTotal / parseInt(people.target.value)
-  
-
-  
-  // const increasePeople = () => {
-  //   setPeople(count => count + 1);
-  // }
-  // const decreasePeople = () => {
-  //   if (people !== 0)
-  //   setPeople(count => count - 1);
-  // }
 
   
   
@@ -86,32 +74,21 @@ const Home = () => {
 
   return (
     <div className = {styles.homeContainer}>
-      {/* <Modal */}
-      {/* // onClose={() => setOpenModal(false)}
-      // onOpen={() => setOpenModal(true)}
-      // className = {styles.modal}
-      // size = {'large'}
-      // trigger={<Button>Show Modal</Button>}>
-      //   <Modal.Header>Bill and Tip Calculator</Modal.Header> */}
+      <Header as='h2'>
+        <Image circular src='https://react.semantic-ui.com/images/avatar/large/patrick.png' /> Hi, there
+      </Header>
       <Container  textAlign='left' className = {styles.container} >
         <Segment placeholder>
               <Grid.Column stretched='true' stackable = 'false'>
-              {/* <Input labelPosition='right' type='text' placeholder='Amount' >
-              <Label basic>£</Label>
-              <input />
-              <Label></Label>
-              </Input> */}
-                
               <Input type='number' placeholder='Number of people...'  onChange= {setPeople}/>
               <datalist id='Number'>
                 {number.map((item) => {
                   return <option value={item}>{Item}</option>
                 })}
               </datalist>
-        
               <Divider></Divider>
               <Input labelPosition='right' type='number' placeholder='Amount' onChange= {setAmount}>
-              <Label basic>£</Label>
+              <Label basic>{defaultCurrency}</Label>
               <input />
               <Label></Label>
               </Input>
@@ -123,38 +100,81 @@ const Home = () => {
               </Button.Group>
               <Divider></Divider>
             
-                <Progress size = {'large'}  progress = {`value${'%'}`} className={styles.progressBar} percent={percentage} indicating />
+                <Progress size = {'large'} font progress = {`value${'%'}`} className={styles.progressBar} percent={percentage} indicating />
               </Grid.Column>
               <Divider></Divider>
 
               <Grid.Column>
-              <Header  as='h3'>Calculate Bill and Tip</Header>
-              <Button.Group  size='huge'>
-              <Button disabled={people === undefined || amount === undefined ? true : false} color = 'instagram' onClick={() => caculateBill()} >Calculate</Button>
-              {/* <Button.Or />
-              <Button color='black' >Custom</Button> */}
+                <Header  as='h3'>Calculate Bill and Tip</Header>
+                <Button.Group  size='huge'>
+                <Button disabled={people === undefined || amount === undefined || people === 0 || amount === 0 ? true : false} color = 'instagram' onClick={() => caculateBill()} >Calculate</Button>
+                    <Modal
+                    size = 'tiny'
+                    onClose={() => setOpen(false)}
+                    onOpen={() => setOpen(true)}
+                    open={open}
+                    trigger={<Button><Icon name = 'setting'></Icon> Settings</Button>}
+                    >
+                    <Modal.Header>Settings</Modal.Header>
+                    <Modal.Content>
+                      <Modal.Description>
+                      <Dropdown
+                          text='Currency'
+                          icon='money'
+                          floating
+                          labeled
+                          button
+                          className='icon'
+                        >
+                          <Dropdown.Menu>
+                            <Dropdown.Header icon='tags' content='Choose Currency' />
+                            <Dropdown.Divider />
+                            <Dropdown.Item onClick  = {() => setDefaultCurrency('£')}
+                              label='£'
+                              text='Sterling'
+                            />
+                            <Dropdown.Item onClick  = {() => setDefaultCurrency('€')}
+                              label='€'
+                              text='Euro'
+                            />
+                            <Dropdown.Item onClick  = {() => setDefaultCurrency('$')}
+                              label='$'
+                              text='Dollar'
+                            />
+                          </Dropdown.Menu>
+                        </Dropdown>
+                        <Dropdown
+                          text='Bill type'
+                          icon='group'
+                          floating
+                          labeled
+                          button
+                          className='icon'
+                        >
+                          <Dropdown.Menu>
+                            <Dropdown.Header icon='tags' content='Choose Bill type' />
+                            <Dropdown.Divider />
+                            
+                          </Dropdown.Menu>
+                        </Dropdown>
+                        
+                      </Modal.Description>
+                    </Modal.Content>
+                    <Modal.Actions>
+                      <Button
+                        content="Confirm"
+                        labelPosition='right'
+                        icon='checkmark'
+                        onClick={() => setOpen(false)}
+                        positive
+                      />
+                    </Modal.Actions>
+                  </Modal>
               </Button.Group>
-              <Message >{`£${showBill == undefined ? 0 : showBill}`}</Message>
-
-              </Grid.Column>
+              <Message >{`${showBill == undefined ? 0 : showBill}`}</Message>
+            </Grid.Column>
           </Segment>
           </Container>
-
-    {/* </Modal> */}
-
-
-
-
-
-
-
-      <div>
-      {/* <Button onClick={increasePercentageTip}>Increment</Button>
-      <Progress progress = {`value${'%'}`} className={styles.progressBar} percent={percentage} indicating /> */}
-      {/* <Progress className= {styles.progressBar} color = {'green'} active={true} size = {'medium'} /> */}
-      </div>
-
-
     </div>
   );
 };
